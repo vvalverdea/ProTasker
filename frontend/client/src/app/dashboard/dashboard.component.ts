ddb33a1b8b0bfd59df6a9b78e7cdefea58158768
vvalverdea <cohetecoder@gmail.com>
2024-12-03 12:04:37 +0100

    feat: card's build as atom

--- frontend/client/src/app/dashboard/dashboard.component.ts
+++ frontend/client/src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { CardComponent } from '../atoms/card/card.component';
+import { ButtonComponent } from '../atoms/button/button.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [ButtonComponent, CardComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
