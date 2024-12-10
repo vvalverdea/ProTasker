cf19eceba3ca166ba57d621d676437d082524933
vvalverdea <cohetecoder@gmail.com>
2024-12-10 07:10:05 +0100

    test: fix tests

--- frontend/client/src/app/mocks/task.ts
+++ frontend/client/src/app/mocks/task.ts
@@ -1,6 +1,4 @@
 export const taskMock = {
-  id: 1,
   title: 'Task 1',
-  description: 'Description 1',
   status: 1,
 };
--- frontend/client/src/app/services/tasks.service.spec.ts
+++ frontend/client/src/app/services/tasks.service.spec.ts
@@ -1,23 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { TasksService } from './tasks.service';
-import {
-  connectFirestoreEmulator,
-  enableIndexedDbPersistence,
-  Firestore,
-  getFirestore,
-  provideFirestore,
-} from '@angular/fire/firestore';
-import { collection, addDoc } from '@angular/fire/firestore';
-import Task from '../interfaces/tasks';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { Firestore } from '@angular/fire/firestore';
 
 describe('TasksService', () => {
   let service: TasksService;
-  let firestoreMock: Firestore;
 
   beforeEach(() => {
-    // Mock Firestore con spies
-    firestoreMock = jasmine.createSpyObj('Firestore', ['collection']);
+    const firestoreMock = jasmine.createSpyObj('Firestore', ['collection']);
     TestBed.configureTestingModule({
       providers: [
         TasksService,
