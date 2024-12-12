ee17b7390e1a9878276c753610f481bd3c63ddb6
vvalverdea <cohetecoder@gmail.com>
2024-12-12 09:22:48 +0100

    feat: vercel.json created

--- esbuild.config.js
+++
@@ -1,9 +0,0 @@
-require('esbuild')
-  .build({
-    entryPoints: ['backend/server.js'],
-    bundle: true,
-    platform: 'node',
-    external: ['mongoose', 'express', 'cors'],
-    outdir: 'dist',
-  })
-  .catch(() => process.exit(1));
