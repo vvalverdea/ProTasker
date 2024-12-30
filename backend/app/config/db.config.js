df6e6854ffdc089b580bfc997df54d27c6976d14
vvalverdea <cohetecoder@gmail.com>
2024-12-30 16:03:33 +0100

    feat: configuring vercel

--- backend/app/config/db.config.js
+++ backend/app/config/db.config.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
 
 module.exports = {
-  url: `mongodb+srv://${process.env.DATABASE_NAME}:${process.env.DATABASE_PASSWORD}@cluster0.sm2cp.mongodb.net/protasker?retryWrites=true&w=majority&appName=Cluster0`,
+  url: `mongodb+srv://${process.env.DATABASE_NAME}:${process.env.DATABASE_PASSWORD}${process.env.MONGODB_URI}`,
 };
